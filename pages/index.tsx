ab2cd8a6a573b282d0ded846b7c3200899916731
Lucas Verdade <lucas.verdade@grad.ufsc.br>
2022-04-10 16:58:18 -0300

    feat: add 2 chart examples

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,40 @@
-import BaseLayout from '@/components/layouts/base/BaseLayout';
-import NavbarLayout from '@/components/layouts/navbar/NavbarLayout';
-import { NextPageWithLayout } from './page'
+import BaseLayout from "@/components/layouts/base/BaseLayout";
+import NavbarLayout from "@/components/layouts/navbar/NavbarLayout";
+import dynamic from "next/dynamic";
+import { NextPageWithLayout } from "./page";
+const LineChartExample = dynamic(()=> import ('../components/charts/ResponsiveLineChartExample'), {ssr:false})
+const PieChartExample = dynamic(()=> import ('../components/charts/PieChartExample'), {ssr:false})
+import { Col, Row } from "react-bootstrap";
 
 const Home: NextPageWithLayout = () => {
-  return (<>Welcome</>);
+  return (
+    <>
+      <Row>
+        <Col>
+          <div style={{height: 218, width: 788}}>
+            <LineChartExample/>
+          </div>
+        </Col>
+        <Col>
+          <div style={{height: 330, width: 330 }}>
+            <PieChartExample/>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div style={{height: 512, width: 512}}>
+            <PieChartExample/>
+          </div>
+        </Col>
+        <Col>
+          <div style={{height: 512, width: 512}}>
+            <LineChartExample/>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Home;
@@ -12,9 +43,7 @@ Home.getLayout = (page) => {
   return (
     <>
       <NavbarLayout />
-      <BaseLayout title="Home">
-        {page}
-      </BaseLayout>
+      <BaseLayout title="Home">{page}</BaseLayout>
     </>
   );
 };
