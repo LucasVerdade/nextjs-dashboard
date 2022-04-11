71313b7876a2653c2e98719285a05b1f68a89136
Lucas Verdade <lucas.verdade@grad.ufsc.br>
2022-04-11 20:42:41 -0300

    fix: layout and styles

--- components/layouts/navbar/NavbarLayout.tsx
+++ components/layouts/navbar/NavbarLayout.tsx
@@ -10,13 +10,22 @@ const NavbarLayout: React.FC<INavbarLayout> = (args) => {
   return (
     <>
       <Navbar>
-        <Container>
+        <Container className={styles.container}>
           <Navbar.Brand href="#" style={{height: '100%', position: 'relative'}}>
-            <Image src="/img/Leaf Main Logo.png" alt="Leaf logo" height={40} width={100}/>
+            <Image src="/img/Leaf Main Logo.png" alt="Leaf logo" height={40} width="100%"/>
           </Navbar.Brand>
           <Nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
+            <Nav.Link>
+              <Link href="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link href="/about">About</Link>
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link>
+              <Link href="/login">Cliente</Link>
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
